c176e55621c370283fc3231965bee05ed0e00bd5
Romanyerom <Romanyerom@github.com>
2024-06-17 11:39:35 +0000

    Fix linting errors in 2-nested.js

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,4 +1,20 @@
 export default function accessImmutableObject(object, array) {
-  return array.reduce((acc, key) => acc && acc[key], object);
+  return array.reduce((acc, key) => {
+    if (acc === undefined) {
+      return undefined;
+    }
+    return acc[key];
+  }, object);
 }
 
+// Test cases
+const obj = {
+  name: {
+    first: 'Guillaume',
+    last: 'Salva',
+  },
+};
+
+console.log(accessImmutableObject(obj, ['name', 'first'])); // Output: 'Guillaume'
+console.log(accessImmutableObject(obj, ['name', 'middle'])); // Output: undefined
+console.log(accessImmutableObject(obj, ['age'])); // Output: undefined
