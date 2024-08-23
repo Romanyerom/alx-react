715a3ed6db2f6317aeef80a14d327ab4c18393cb
Roman Zerihun <125880809+Romanyerom@users.noreply.github.com>
2024-08-23 13:30:21 +0300

    Update

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,20 +1,19 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append("<p>Copyright - Holberton School</p>");
+
 let count = 0;
 function updateCounter() {
-  count += 1;
-  $('#count').text(`${count} clicks on the button`);
+    //increment the count by 1
+    count += 1;
+
+    // Update the paragraph element with the new count
+    $('#count').text(`${count} clicks on the button`);
 }
 
-const debouncedUpdateCounter = _.debounce(updateCounter, 300);
-
-$(document).ready(function() {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button>Click here to get started</button>');
-  $('body').append("<p id='count'></p>");
-  $('body').append('<p>Copyright - Holberton School</p>');
-
-  $('button').on('click', debouncedUpdateCounter);
-});
+$('button').on('click', _.debounce(updateCounter, 500));
