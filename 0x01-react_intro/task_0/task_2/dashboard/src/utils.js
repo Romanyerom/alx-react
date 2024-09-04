cbd01d831a04bda37c6a35ff68a6fe4eb6c4c754
Roman Zerihun <125880809+Romanyerom@users.noreply.github.com>
2024-09-04 14:29:17 +0300

    Delete 0x01-react_intro/task_0 directory

--- 0x01-react_intro/task_0/task_2/dashboard/src/utils.js
+++
@@ -1,6 +0,0 @@
-/* full-year */
-export const getFullYear = () => new Date().getFullYear();
-
-/* footer-copy */
-export const getFooterCopy = (isIndex) => (isIndex ? "Holberton School" : "Holberton School main dashboard");
-
